3d0ea8ea45667eea1344736f83091d536aceb970
Lucas Siqueira <lucassiqzro@gmail.com>
2019-10-18 10:58:01 -0300

    add auth middleware

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,11 @@ import { Router } from 'express';
 
 import MonitoringController from './app/controllers/MonitoringController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
+
+routes.use(authMiddleware);
 
 routes.get('/monitoring/:name', MonitoringController.index);
 
